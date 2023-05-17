535b37beec6b1c85c685939ccfc11c83c7a28ed8
MariaMuniz <cidamuniz2011@yahoo.com.br>
2023-05-17 15:55:36 -0300

    base criada

---
+++ web/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div className="text-2xl font-bold uppercase bg-red-500 rounded m-10 w-20 h-20">oi</div>
+  )
+}
---
+++ web/components/Button.tsx
@@ -0,0 +1,7 @@
+
+
+export function Button() {
+  return (
+    <h2>oi</h2>
+  )
+}
